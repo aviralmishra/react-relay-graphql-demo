1d052e109b2fb0c75fbb8f7192c928dc4fd068b2
Aviral Mishra <aviral.mishra@gmail.com>
2017-12-18 21:48:21 +1100

    Build and dependencies setup

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # react-flux-graphql-demo
+
 React based data-driven application built with Flux, GraphQL and Relay
+
+## Pre Reqs
+
+[PM2](https://www.npmjs.com/package/pm2)
+
+```
+npm install pm2 -g
+```
+
+## Installation
+
+```
+git clone git@github.com:aviralmishra/react-flux-graphql-demo.git
+
+npm install
+```
+
+## Dev Server Run
+
+```
+npm run dev
+```
+
+## Dev Web Run & Watch
+
+```
+npm run webpack
+```
+
+## Test
+
+```
+npm run test
+```
+
+## Test Coverage
+
+```
+npm run verify-tests
+```
+
+## Prod Build
+
+```
+npm run build-webpack
+npm run build-node
+```
+
+## Prod Run
+
+```
+npm run start-prod
+```
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+  resolve: {
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+  },
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
+        }
+      }, {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      }, {
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000'
+      }, {
+        test: /\.jpg$/,
+        loader: 'file-loader'
+      }, {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader? limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+  plugins: [new webpack.optimize.CommonsChunkPlugin({name: 'vendor'})]
+};
+
+module.exports = config;
